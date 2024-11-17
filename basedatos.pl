d42058250bc477396cb3d190c3b3b033bc2e70a3
Cristhian <cristhian.bravo.8657@gmail.com>
2024-11-16 22:43:55 -0500

    Script Perl adaptado al username y password

--- basedatos.pl
+++ basedatos.pl
@@ -5,27 +5,27 @@ use DBI;
 
 # Configuración de conexión
 my $database = "prueba";
-my $hostname = "mariadb2";         # Nombre del contenedor MariaDB
-my $port     = 3307;               # Puerto predeterminado de MariaDB
-my $user     = "root";             # Usuario de MariaDB
-my $password = "tu_contraseña_segura"; # Contraseña para el usuario root
-my $username = "cgi_user";
-my $password = "tu_password";
+my $hostname = "db";              # Cambiar a "db", el nombre del servicio en Docker Compose
+my $port     = 3306;              # Cambiar a 3306, el puerto predeterminado de MariaDB
+my $username = "cgi_user";        # Nombre de usuario actualizado
+my $password = "tu_password";     # Contraseña actualizada
 
 # DSN de conexión
 my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
 
 # Conectar a la base de datos
-my $dbh = DBI->connect($dsn, $user, $password, {
+my $dbh = DBI->connect($dsn, $username, $password, {
     RaiseError       => 1,
     PrintError       => 0,
     mysql_enable_utf8 => 1,
 });
 
+# Validar conexión
 if ($dbh) {
+    print "Content-type: text/plain\n\n";
     print "Conexión exitosa a la base de datos '$database'.\n";
 } else {
-    die "Error al conectar a la base de datos: $DBI::errstr\n";
+    die "Content-type: text/plain\n\nError al conectar a la base de datos: $DBI::errstr\n";
 }
 
 # Consulta de prueba
@@ -34,15 +34,15 @@ my $sth = $dbh->prepare($query);
 $sth->execute();
 
 # Imprimir resultados de la consulta
+print "Resultados de la tabla 'peliculas':\n";
 while (my @row = $sth->fetchrow_array) {
     print join(", ", @row), "\n";
 }
 
-# Insertar un registro
-my $sth = $dbh->prepare("INSERT INTO Actor( Name) VALUES (?)");
-$sth->execute("Jhonny Deep");
+# Insertar un registro en la tabla 'actores'
+$sth = $dbh->prepare("INSERT INTO actores (nombre) VALUES (?)");
+$sth->execute("Johnny Depp");
 
 # Cerrar la conexión
 $sth->finish();
 $dbh->disconnect();
-
